152c3210e6894ce7a1f4cd17dcc078a12aa762dc
TurnUpTheMike <michael.pantaleano@wpengine.com>
2018-05-26 18:00:54 -0500

    all letters pants way

--- camo/solution/answerkeypants.py
+++ camo/solution/answerkeypants.py
@@ -1,23 +1,45 @@
 from solution.answerkey import AnswerKeyGenerator, AnswerKey
 from solution.puzzleutility import PuzzleUtility
 import re
+import random
+
+
+class AnswerKeyPants(AnswerKey):
+    def __init__(self):
+        super().__init__()
+        self.answers_unmodified = {}
+
+    def print_answerkey(self):
+        print("------------{}-----------".format(self.__class__.__name__))
+        for letter in "abcdefghijklmnopqrstuvwxyz":
+            print("{}: {}".format(letter, self.answers_unmodified[letter]))
+        print("--------------------------------------")
 
 
 class AnswerKeyGeneratorPants(AnswerKeyGenerator):
-    def generate_answer_key(self, wordbank):
-        print("AnswerkeyGeneratorPants")
-        answerkey = AnswerKey()
+    def __init__(self, properties, utility):
+        super().__init__(properties, utility)
+        self.flag_verbose = False
+        self.flag_match_counts = True
 
-        word_a = wordbank.hash_by_letter['a'].pop()
-        self.choose_word('a', word_a, wordbank)
-        print("")
-        print("")
-        print("")
+    def log(self, message):
+        if self.flag_verbose:
+            print(message)
+
+    def log_matches(self, message):
+        if self.flag_match_counts:
+            print(message)
+
+    def generate_answer_key(self, wordbank):
+        self.log("AnswerkeyGeneratorPants")
+        answerkey = AnswerKeyPants()
 
         for letter in "abcdefghijklmnopqrstuvwxyz":
+            self.log("Choosing letter {}".format(letter))
             letter_set = wordbank.hash_by_letter[letter]
             word = letter_set.pop()
-            answerkey.answers[letter] = word
+            answerkey.answers[letter] = self.choose_word(letter, word, wordbank)
+            answerkey.answers_unmodified[letter] = word
             wordbank.remove_word(word)
 
         return answerkey
@@ -29,13 +51,18 @@ class AnswerKeyGeneratorPants(AnswerKeyGenerator):
             return word
 
         # choose a word
-        detterent = word_matches.values()[0]
+        self.log("Chosen ({}) {} had {} matches".format(letter, word, len(word_matches)))
+        self.log_matches("Chosen ({}) {} had {} matches".format(letter, word, len(word_matches)))
+        match_values = list(word_matches.values())
+        random_index = random.randint(0, len(match_values) - 1)
+        deterrent = match_values[random_index]
 
+        # combine the deterrent with the chosen word
         letter_index = self.util.letter_ndx_of_word(word, letter)
         word_prefix = word[:letter_index]
         word_suffix = word[letter_index + 1:]
         prefix_group = deterrent.groups()[0]
-        suffix_group = detterent.groups()[2]
+        suffix_group = deterrent.groups()[2]
 
         prefix_to_use = word_prefix
         if prefix_group.find(word_prefix) > -1:
@@ -55,24 +82,23 @@ class AnswerKeyGeneratorPants(AnswerKeyGenerator):
         word_prefix = word[:letter_index]
         word_suffix = word[letter_index + 1:]
 
-        wordbank.print_letterbank(letter)
+        # wordbank.print_letterbank(letter)
 
-        print("Choose {} for the letter {} ".format(word, letter))
-        print(word_prefix)
-        print(word_suffix)
+        self.log("GetWordsThatAlmostMatch {} for the letter {} ".format(word, letter))
+        # self.log(word_prefix)
+        # self.log(word_suffix)
 
         prefix_expression = self.generate_prefix_expression(word_prefix)
-        print(prefix_expression)
+        # self.log(prefix_expression)
         suffix_expression = self.generate_suffix_expression(word_suffix)
-        print(suffix_expression)
+        # self.log(suffix_expression)
 
         word_length = len(word)
         num_letters_on_left_side = self.util.chosen_letter_index - letter_index
         num_letters_on_right_side = self.util.chosen_letter_index - (word_length - 1 - letter_index)
-        print("letters on side {}".format(num_letters_on_left_side))
+        # self.log("letters on side {}".format(num_letters_on_left_side))
 
         # words that end in the prefix
-        print("all the words that match")
         chosen_letter_expression = self.generate_not_letter_expression(letter)
         program = re.compile(
             "((?:[a-z]{{0,{}}}{}|^))({})((?:$|{}[a-z]{{0,{}}}))".format(num_letters_on_left_side,
@@ -85,8 +111,8 @@ class AnswerKeyGeneratorPants(AnswerKeyGenerator):
         for word in wordbank.unique_words:
             match_obj = program.match(word)
             if match_obj:
-                print("Found matching word {}".format(word))
-                print(match_obj.groups())
+                # self.log("Found matching word {}".format(word))
+                self.log(match_obj.groups())
                 found_words[word] = match_obj
 
         return found_words
--- camo/solution/test/test_answerkeypants.py
+++ camo/solution/test/test_answerkeypants.py
@@ -8,6 +8,8 @@ from parameterized import parameterized
 class Properties:
     def __init__(self):
         self.puzzle_row_length = 13
+        self.min_word_length = 5
+        self.max_word_length = 13
 
 
 class TestAnswerKeyPants(unittest.TestCase):
@@ -105,19 +107,134 @@ class TestAnswerKeyPants(unittest.TestCase):
         self.assertIn('zzzzabz', match_hash)
         self.assertNotIn('yzzzzzzabz', match_hash)
 
-    def xtest_choose_word_planet(self):
+    def test_choose_word_planet(self):
         chosen_letter = 'a'
         chosen_word = 'planet'
 
-        self.wordbank = WordBank(self.properties)
         self.wordbank.unique_words.add("apple")
         self.wordbank.unique_words.add("sample")
 
-        match_hash = self.generator.get_words_that_almost_match(chosen_letter, chosen_word, self.wordbank)
+        mashed_word = self.generator.choose_word(chosen_letter, chosen_word, self.wordbank)
 
-        self.assertEqual(1, 1)
+        self.assertEqual('applanet', mashed_word)
 
+    def test_generate_answer_key(self):
+        # A
+        self.wordbank.add_word("planet")
+        self.wordbank.add_word("apple")
+        self.wordbank.add_word("sample")
+        # B
+        self.wordbank.add_word("bubble")
+        self.wordbank.add_word("trouble")
+        self.wordbank.add_word("thumb")
+        # C
+        self.wordbank.add_word("practice")
+        self.wordbank.add_word("cactus")
+        self.wordbank.add_word("tractor")
+        # D
+        self.wordbank.add_word("candle")
+        self.wordbank.add_word("bland")
+        self.wordbank.add_word("doctor")
+        # E
+        self.wordbank.add_word("treacherous")
+        self.wordbank.add_word("eternity")
+        self.wordbank.add_word("episode")
+        # F
+        self.wordbank.add_word("perfect")
+        self.wordbank.add_word("feather")
+        self.wordbank.add_word("staff")
+        # G
+        self.wordbank.add_word("laugh")
+        self.wordbank.add_word("gather")
+        self.wordbank.add_word("lugage")
+        # H
+        self.wordbank.add_word("beach")
+        self.wordbank.add_word("hopeful")
+        self.wordbank.add_word("brother")
+        # I
+        self.wordbank.add_word("guitar")
+        self.wordbank.add_word("instinct")
+        self.wordbank.add_word("light")
+        # J
+        self.wordbank.add_word("jungle")
+        self.wordbank.add_word("project")
+        self.wordbank.add_word("justice")
+        self.wordbank.add_word("jester")
+        # K
+        self.wordbank.add_word("blanket")
+        self.wordbank.add_word("clock")
+        self.wordbank.add_word("trick")
+        # L
+        self.wordbank.add_word("color")
+        # M
+        self.wordbank.add_word("smile")
+        # N
+        self.wordbank.add_word("stamina")
+        # O
+        self.wordbank.add_word("blood")
+        # P
+        self.wordbank.add_word("protect")
+        # Q
+        self.wordbank.add_word("sequence")
+        self.wordbank.add_word("queen")
+        self.wordbank.add_word("liquor")
+        # R
+        self.wordbank.add_word("partner")
+        # S
+        self.wordbank.add_word("respect")
+        # T
+        self.wordbank.add_word("flatten")
+        # U
+        self.wordbank.add_word("hundred")
+        self.wordbank.add_word("unique")
+        self.wordbank.add_word("plateau")
+        # V
+        self.wordbank.add_word("alive")
+        self.wordbank.add_word("volcano")
+        self.wordbank.add_word("vivid")
+        # W
+        self.wordbank.add_word("below")
+        self.wordbank.add_word("weather")
+        self.wordbank.add_word("whether")
+        self.wordbank.add_word("world")
+        self.wordbank.add_word("wealth")
+        self.wordbank.add_word("power")
+        # X
+        self.wordbank.add_word("expert")
+        self.wordbank.add_word("extra")
+        self.wordbank.add_word("suffix")
+        # Y
+        self.wordbank.add_word("player")
+        self.wordbank.add_word("yellow")
+        self.wordbank.add_word("portray")
+        self.wordbank.add_word("belly")
+        self.wordbank.add_word("young")
+        self.wordbank.add_word("pantry")
+        # Z
+        self.wordbank.add_word("zebra")
+        self.wordbank.add_word("pizza")
+        self.wordbank.add_word("puzzle")
+        self.wordbank.add_word("plaza")
 
+        # self.wordbank.print_wordbank()
+
+        # Generate the AnswerKey
+        # self.generator.flag_verbose = True
+        self.generator.flag_match_counts = True
+        answerkey = self.generator.generate_answer_key(self.wordbank)
+
+        # answerkey.print_answerkey()
+
+        # assert we have answers for all letters
+        for letter in "abcdefghijklmnopqrstuvwxyz":
+            self.assertIn(letter, answerkey.answers)
+            self.assertIn(letter, answerkey.answers_unmodified)
+
+            # the real answer is a substring of the mashed up answer
+            real_answer = answerkey.answers_unmodified[letter]
+            mashed_answer = answerkey.answers[letter]
+            # print("{} : {}".format(letter, mashed_answer))
+            self.assertIn(real_answer, mashed_answer)
 
 
 
